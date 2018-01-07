10047a48c547037a2a0fd388e21e61ae6009c67c
Tarciso <tarcisoloiola@hotmail.com>
2018-01-07 10:12:01 -0500

    add yarn.lock

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,7 +3,7 @@ import { Container, Row } from "../../components/Grid";
 import { FixedHeader } from "../../components/Header";
 import { Body } from "../../components/Body";
 import { Login, Register } from "../../components/Modal";
-import { H1 } from "../../components/Tag";
+import { H1, Margin } from "../../components/Tag";
 import { Button } from "../../components/Button";
 import { Logo } from "../../components/Logo";
 import { NavButton } from "../../components/Nav";
@@ -11,24 +11,24 @@ import { NavButton } from "../../components/Nav";
 const Home = () =>
 <div>
     <Body>
-    <div className="fixed-top fixed-top-custom"></div>
-    <FixedHeader>
-            <Container>
-                <Row>
-                    <Logo/>
-                    <NavButton>
-                        <H1>
-                        <Button toggle="modal" target="#login">Login</Button>
-                        <Button toggle="modal" target="#register">Register</Button>
-                        </H1>
-                    </NavButton>
-                </Row>
-            </Container>
-    </FixedHeader>
-    <Login/>
-    <Register/>
-        {/* <div className="margin-top"></div>
-        <a href="/findproperty" className="btn btn-large-success">Find a new home.</a> */}
+        <div className="fixed-top fixed-top-custom"></div>
+        <FixedHeader>
+                <Container>
+                    <Row>
+                        <Logo/>
+                        <NavButton>
+                            <H1>
+                                <Button toggle="modal" target="#login">Login</Button>
+                                <Button toggle="modal" target="#register">Register</Button>
+                            </H1>
+                        </NavButton>
+                    </Row>
+                </Container>
+        </FixedHeader>
+        <Login/>
+        <Register/>
+        <Margin/>
+        <a href="/findproperty" className="btn btn-large-success">Find a new home.</a>
     </Body>
 </div>
 
