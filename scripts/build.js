52bc6927495989719a265e13fdaf3994a414e284
Tarciso <tarcisoloiola@hotmail.com>
2018-01-23 20:16:44 -0500

    Changed build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,3 +1,12 @@
-const args = ["run build"];
-const opts = { stdio: "inherit", cwd: "client", shell: true };
-require("child_process").spawn("npm", args, opts);
+// let args = ["install"];
+// let opts = { stdio: "inherit", cwd: "client", shell: true };
+// require("child_process").spawn("npm", args, opts);
+
+// args = ["run build"];
+// opts = { stdio: "inherit", cwd: "client", shell: true };
+// require("child_process").spawn("npm", args, opts);
+
+const exec = require("child_process").exec;
+
+process.chdir('client');
+return exec('npm install & npm run build');
