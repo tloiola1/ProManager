54b268aa45cbd59026faa63e9a90f9484ddc7d5c
Tarciso <tarcisoloiola@hotmail.com>
2018-01-28 22:09:40 -0500

    commit to heroku

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -16,21 +16,7 @@ module.exports = {
     const textId = req.body.messageId;
     db.Property
       .update({_id: req.params._id}, {$pull: { message: {_id: textId }}})//, {message: {$elemMatch: {_id : req.body.messageId}}}
-      .then((property) => {
-        
-        // console.log("Before");
-        // console.log(property.message[0]._id)
-        // console.log("Middle");        
-        // console.log(textId);
-        // console.log("After");                
-        // for(let i = 0; i < property.message.length; i++){
-
-        //   if(property.message[i]._id === textId){
-        //     console.log("GREAT!!");
-        //     console.log(property.message[i]._id);
-        //   }
-        // }
-        // console.log("Beyond");                        
+      .then((property) => {                      
         res.json(property);
       })
       .catch(err => res.status(422).json(err));
